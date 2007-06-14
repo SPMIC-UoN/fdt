eded629363d6484ec6070095ef084a94cc3b11c2
Duncan Mortimer <duncan.mortimer@ndcn.ox.ac.uk>
2007-06-14 14:50:21 +0000

    Merge of Darwin Universal Build (Take 2)

---
+++ dpm.cc
@@ -0,0 +1,42 @@
+/*  Copyright (C) 2007 University of Oxford  */
+
+/* S. Jbabdi */
+
+/*  CCOPYRIGHT  */
+
+#include <iostream>
+#include <cmath>
+
+#include "dpm_gibbs.h"
+
+using namespace NEWMAT;
+using namespace DPM;
+using namespace Utilities;
+
+int main (int argc, char *argv[]){
+
+  Log& logger = LogSingleton::getInstance();
+
+  dpmOptions& opts = dpmOptions::getInstance();
+  opts.parse_command_line(argc,argv,logger);
+
+  // read input files
+  Matrix data; // data is nxd
+  data=read_ascii_matrix(opts.datafile.value());
+
+  
+  // create Gibb's sampler instance
+  //cout <<"instanciate DPM_GibbsSampler"<<endl;
+  DPM_GibbsSampler gs(data,
+		      opts.numiter.value(),opts.burnin.value(),opts.sampleevery.value());
+
+  //cout<<"initialisation"<<endl;
+  gs.init();
+  //cout<<"running..."<<endl;
+  gs.run();
+
+
+  // save output files
+  gs.save();
+}
+
