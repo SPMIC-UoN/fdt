13e5bcd67264c162b14275f6a14f172bf48e839f
David Flitney <dave.flitney@ndcn.ox.ac.uk>
2007-07-19 13:31:30 +0000

    *** empty log message ***

--- bedpostx_single_slice.sh
+++ bedpostx_single_slice.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 
-subjdir=$1
-slice=`expr $SGE_TASK_ID - 1`
-slicezp=`${FSLDIR}/bin/zeropad $slice 4`
-
-#$ -cwd -q long.q
+#$ -cwd 
+#$ -q long.q
 #$ -S /bin/sh
-#$ -V -N bedpostX
+#$ -V
+#$ -N bedpostx
 #$ -m a
+#$ -o $1.bedpostX/logs -e $1.bedpostX/logs
+
+subjdir=$1
+nfibres=$2
+fudge=$3
+bi=$4
+slice=$5
+
+slicezp=`${FSLDIR}/bin/zeropad $slice 4`
 
 ${FSLDIR}/bin/xfibres\
  --data=$subjdir/data_slice_$slicezp\
  --mask=$subjdir/nodif_brain_mask_slice_$slicezp\
  -b $subjdir/bvals -r $subjdir/bvecs\
  --forcedir --logdir=$subjdir.bedpostX/diff_slices/data_slice_$slicezp\
- --fudge=$3 --nj=5000 --bi=$4 --se=100 --upe=24 --nfibres=$2 > $subjdir.bedpostX/logs/log$slicezp  && echo Done
+ --fudge=$fudge --nj=5000 --bi=$bi --se=100 --upe=24 --nfibres=$nfibres > $subjdir.bedpostX/logs/log$slicezp  && echo Done
 
