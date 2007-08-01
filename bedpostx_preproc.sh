3d45978b8a0a016dd85230b9b01aed737a0bd296
Saad Jbabdi <saad@fmrib.ox.ac.uk>
2007-08-01 17:24:03 +0000

    remove nodif

--- bedpostx_preproc.sh
+++ bedpostx_preproc.sh
@@ -11,7 +11,7 @@ subjdir=$1
 
 echo Copying files to bedpost directory
 cp ${subjdir}/bvecs ${subjdir}/bvals ${subjdir}.bedpostX
-${FSLDIR}/bin/imcp ${subjdir}/nodif ${subjdir}/nodif_brain_mask ${subjdir}.bedpostX
+${FSLDIR}/bin/imcp ${subjdir}/nodif_brain_mask ${subjdir}.bedpostX
 ${FSLDIR}/bin/fslmaths\
  ${subjdir}/nodif\
  -mas ${subjdir}/nodif_brain_mask\
