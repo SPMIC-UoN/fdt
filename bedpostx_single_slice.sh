405b78638d8fac74cec6777e645533ffba806a33
Saad Jbabdi <saad@fmrib.ox.ac.uk>
2007-10-03 12:40:35 +0000

    added two options to bedpostx

--- bedpostx_single_slice.sh
+++ bedpostx_single_slice.sh
@@ -12,7 +12,10 @@ subjdir=$1
 nfibres=$2
 fudge=$3
 bi=$4
-slice=$5
+njumps=$5
+sampleevery=$6
+slice=$7
+
 
 slicezp=`${FSLDIR}/bin/zeropad $slice 4`
 
@@ -21,5 +24,5 @@ ${FSLDIR}/bin/xfibres\
  --mask=$subjdir/nodif_brain_mask_slice_$slicezp\
  -b $subjdir/bvals -r $subjdir/bvecs\
  --forcedir --logdir=$subjdir.bedpostX/diff_slices/data_slice_$slicezp\
- --fudge=$fudge --nj=1250 --bi=$bi --se=25 --upe=24 --nfibres=$nfibres > $subjdir.bedpostX/logs/log$slicezp  && echo Done
+ --fudge=$fudge --nj=$njumps --bi=$bi --se=$sampleevery --upe=24 --nfibres=$nfibres > $subjdir.bedpostX/logs/log$slicezp  && echo Done
 
