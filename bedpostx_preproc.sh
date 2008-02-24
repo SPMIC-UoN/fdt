828035e9a715fc747be1dd844ca558d136e3cb27
Stephen Smith <steve@fmrib.ox.ac.uk>
2008-02-24 12:37:51 +0000

    fixed silly bugs - bad boys!

--- bedpostx_preproc.sh
+++ bedpostx_preproc.sh
@@ -5,10 +5,10 @@ subjdir=$1
 echo Copying files to bedpost directory
 cp ${subjdir}/bvecs ${subjdir}/bvals ${subjdir}.bedpostX
 ${FSLDIR}/bin/imcp ${subjdir}/nodif_brain_mask ${subjdir}.bedpostX
-${FSLDIR}/bin/fslmaths\
- ${subjdir}/nodif\
- -mas ${subjdir}/nodif_brain_mask\
- ${subjdir}.bedpostX/nodif_brain
+
+if [ `${FSLDIR}/bin/imtest ${subjdir}/nodif` = 1 ] ; then
+    ${FSLDIR}/bin/fslmaths ${subjdir}/nodif -mas ${subjdir}/nodif_brain_mask ${subjdir}.bedpostX/nodif_brain
+fi
 
 ${FSLDIR}/bin/fslslice ${subjdir}/data
 ${FSLDIR}/bin/fslslice ${subjdir}/nodif_brain_mask
