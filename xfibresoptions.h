c92d6f465e1a835d5c22dfc9ce2273ee16fdeb6f
Stamatios Sotiropoulos <stam@fmrib.ox.ac.uk>
2012-07-14 13:58:59 +0000

    Added grad-nonlinearity option. Made rician, model2, f0 visible

--- xfibresoptions.h
+++ xfibresoptions.h
@@ -36,7 +36,7 @@ class xfibresOptions {
   Option<string> bvecsfile;
   Option<string> bvalsfile;
   Option<int> nfibres;
-  FmribOption<int> modelnum;
+  Option<int> modelnum;
   Option<float> fudge;
   Option<int> njumps;
   Option<int> nburn;
@@ -49,9 +49,10 @@ class xfibresOptions {
   Option<bool> localinit;
   Option<bool> nonlin;
   Option<bool> cnonlin;
-  FmribOption<bool> rician;
-  FmribOption<bool> f0;
-  FmribOption<bool> ardf0;
+  Option<bool> rician;
+  Option<bool> f0;
+  Option<bool> ardf0;
+  Option<string> grad_file;
 
   void parse_command_line(int argc, char** argv,  Log& logger);
   
@@ -135,6 +136,9 @@ class xfibresOptions {
    rician(string("--rician"),false,string("Use Rician noise modelling"),false,no_argument),
    f0(string("--f0"),false,string("Add to the model an unattenuated signal compartment"),false,no_argument),
    ardf0(string("--ardf0"),false,string("Use ard on f0"),false,no_argument),
+   grad_file(string("--gradnonlin"), string("gradnonlin"),
+	     string("Gradient Nonlinearity Tensor file"),
+	     false, requires_argument),  
    options("xfibres v1.11", "xfibres --help (for list of options)\n")
      {
        try {
@@ -163,6 +167,7 @@ class xfibresOptions {
        options.add(rician);
        options.add(f0);
        options.add(ardf0);
+       options.add(grad_file);
      }
      catch(X_OptionError& e) {
        options.usage();
