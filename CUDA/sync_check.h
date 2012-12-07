5a76f4ad3382e306729ec7d1fae4a17065ae2be8
Moises Fernandez <moisesf@fmrib.ox.ac.uk>
2012-12-07 20:36:51 +0000

    A function to synchronize and check errors after run each kernel

---
+++ CUDA/sync_check.h
@@ -0,0 +1,19 @@
+#include <fstream>
+
+#define safecall(call) do{\
+	cudaError_t err=call;\
+	if (cudaSuccess != err){\
+		printf("cuda error at %s:%d. %s\n",__FILE__,__LINE__,cudaGetErrorString(err));\
+	}\
+}while(0)
+
+#define sync_check(message) do{;\
+	safecall(cudaDeviceSynchronize());\
+	cudaError_t error = cudaGetLastError();\
+	if (cudaSuccess != error){\
+		printf("ERROR: %s: %s\n",message,cudaGetErrorString(error));\
+		exit(-1);\
+	}\
+}while(0)
+
+
