d27358fd17369e0c7193b95d219c2db62eaac723
Moises Fernandez <moisesf@fmrib.ox.ac.uk>
2013-04-11 15:04:58 +0000

    Deleted dim_blocks.h: Deprecated, now in options/options.h

--- CUDA/dim_blocks.h
+++
@@ -1,9 +0,0 @@
-/*  dim_blocks.h
-
-    Tim Behrens, Saad Jbabdi, Stam Sotiropoulos, Moises Hernandez  - FMRIB Image Analysis Group
-
-    Copyright (C) 2005 University of Oxford  */
-
-/*  CCOPYRIGHT  */
-
-#define THREADS_X_BLOCK_FIT 64
