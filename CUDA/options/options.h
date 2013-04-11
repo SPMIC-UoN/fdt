318e4dbaf8902c893514390dba25ac44c2b148a0
Moises Fernandez <moisesf@fmrib.ox.ac.uk>
2013-04-11 15:33:05 +0000

    Changed to define maximums in the compiled version

--- CUDA/options/options.h
+++ CUDA/options/options.h
@@ -1,5 +1,11 @@
-#define NDIRECTIONS 734 
-#define NFIBRES 3 
-#define NPARAMS 14
-#define MAXNDIRS_PER_THREAD 12
-#define THREADS_BLOCK 64
+#define MAXNFIBRES 4 
+#define MAXNPARAMS 15
+//NFIBRES*3 + S0 + d + d_std + f0 + rician = 15
+#define MAXNDIR 640
+//640 gradient directions
+#define THREADS_BLOCK_MCMC 64
+#define THREADS_BLOCK_FIT 64
+#define MAXNDIRS_PER_THREAD 10 
+//MAXNDIR/THREADS_BLOCK_MCMC
+
+
