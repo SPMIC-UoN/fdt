4d103eb97bbd3377930cef1519ef28b2e7286df8
Moises Fernandez <moisesf@fmrib.ox.ac.uk>
2013-04-11 15:37:34 +0000

    fix number of maximum number of parameters

--- CUDA/options/options.h
+++ CUDA/options/options.h
@@ -1,6 +1,7 @@
 #define MAXNFIBRES 4 
-#define MAXNPARAMS 15
-//NFIBRES*3 + S0 + d + d_std + f0 + rician = 15
+#define MAXNPARAMS 16
+//MAXNFIBRES*3 + S0 + d + d_std + f0 = 16
+//rician only in runmcmc and doesn't use MAXNPARAMS
 #define MAXNDIR 640
 //640 gradient directions
 #define THREADS_BLOCK_MCMC 64
