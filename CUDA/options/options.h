5740dda22ae7971c650fd68fc669dbe158b9b26a
Moises Fernandez <moisesf@fmrib.ox.ac.uk>
2013-05-10 13:46:29 +0000

    Avoid Static Declaration of arrays (moved to shared or Global Memory)

--- CUDA/options/options.h
+++ CUDA/options/options.h
@@ -1,12 +1,4 @@
-#define MAXNFIBRES 4 
-#define MAXNPARAMS 16
-//MAXNFIBRES*3 + S0 + d + d_std + f0 = 16
-//rician only in runmcmc and doesn't use MAXNPARAMS
-#define MAXNDIR 640
-//640 gradient directions
 #define THREADS_BLOCK_MCMC 64
 #define THREADS_BLOCK_FIT 64
-#define MAXNDIRS_PER_THREAD 10 
-//MAXNDIR/THREADS_BLOCK_MCMC
 
 
