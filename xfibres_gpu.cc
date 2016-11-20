80b0d15fe17b531534cb338da8e3ff67d1687af7
Moises Fernandez <moisesf@fmrib.ox.ac.uk>
2016-11-20 13:20:42 +0000

    Delete not used function

--- xfibres_gpu.cc
+++ xfibres_gpu.cc
@@ -19,16 +19,6 @@
 
 using namespace Xfibres;
 
-inline bool isInteger(const std::string & s)
-{
-   if(s.empty() || ((!isdigit(s[0])) && (s[0] != '-') && (s[0] != '+'))) return false ;
-
-   char * p ;
-   strtol(s.c_str(), &p, 10) ;
-
-   return (*p == 0) ;
-}
-
 //////////////////////////////////////////////////////////
 //       XFIBRES CPU PART. IT CALLS TO GPU PART
 //////////////////////////////////////////////////////////
