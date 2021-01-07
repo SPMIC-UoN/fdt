46b849916f4b552e88eabea59dbeb82084d12037
Paul McCarthy <paulmc@fmrib.ox.ac.uk>
2021-01-07 13:29:46 +0000

    BF: Fix improperly quoted command

--- fdt.tcl
+++ fdt.tcl
@@ -889,8 +889,8 @@ proc fdt:apply { w dialog } {
        	    if { $canwrite } {
        		puts "rm -rf $probtrack(output)"
        		exec rm -rf "$probtrack(output)"
-	        puts "mkdir -p "$probtrack(output)""
-		exec mkdir -p $probtrack(output)
+	        puts "mkdir -p $probtrack(output)"
+            exec mkdir -p "$probtrack(output)"
        	    }
 
 	    set filebase $probtrack(output)/fdt
